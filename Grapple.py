325140741f026ed28cffa05b8616197909844737
JonathanDWL <145470703+JonathanDWL@users.noreply.github.com>
2024-01-26 10:13:01 -0600

    py file

---
+++ Grapple.py
@@ -0,0 +1,64 @@
+'''
+Tutorial demonstrates how to create a game window with Python Pygame.
+
+Any pygame program that you create will have this basic code
+'''
+
+import pygame
+import sys
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Player, self).__init__()
+        self.height = 10
+        self.width = 10
+        self.color = (255, 64, 64)
+        self.image = pygame.Surface((self.width, self.height), pygame.SRCALPHA, 32)
+        self.image = self.image.convert_alpha()
+        self.rect = self.image.get_rect(center = (x, y))
+        pygame.draw.ellipse(self.image, self.color, self.rect)
+        self.deltax = 0
+        self.deltay = 0
+        self.x = self.rect.centerx
+        self.y = self.rect.centery
+
+# Initialize Pygame and give access to all the methods in the package
+pygame.init()
+
+# Set up the screen dimensions
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Grapple")
+
+# Define colors
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+# Create clock to later control frame rate
+clock = pygame.time.Clock()
+
+player = Player(400, 300)
+
+# Main game loop
+running = True
+while running:
+    # Event handling
+    for event in pygame.event.get(): # pygame.event.get()
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the screen with a color (e.g., white)
+    screen.fill(WHITE)
+
+    
+
+    # Update the display
+    pygame.display.flip()
+
+    # Set a frame rate to 60 frames per second
+    clock.tick(60)
+
+# Quit Pygame properly
+pygame.quit()
+sys.exit()
